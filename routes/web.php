c47bdbf30b006b1e1922d786859d9932336a4d72
azukoo99 <latifulzakimubarak@gmail.com>
2026-06-11 09:45:47 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
 
+
+Route::get('/seed-db', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+
+    return 'Seeder selesai';
+});
+
 require __DIR__.'/auth.php';
